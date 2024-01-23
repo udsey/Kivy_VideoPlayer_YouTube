c296dba3c2daf8761a4d5de591a633b9c44bf023
udsey <yuuhik@yandex.ru>
2024-01-22 21:51:40 -0300

    update view

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Kivy_VideoPlayer_YouTube
 example of using kivy VideoPlayer to play youtube's video
+
+! Delete ffmpeg package from ./venv/lib/python3.10/site-packages !
--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 from kivy.app import App
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
-from kivy.uix.video import Video
-from kivy.uix.videoplayer import VideoPlayer
-from kivy.uix.label import Label
-from kivy.uix.textinput import TextInput
+from kivy.lang import Builder
 from pytube import YouTube
+from kivy.uix.screenmanager import  Screen
+
+Builder.load_file("main.kv")
 
 
 
-class YouTubePlayerApp(App):
-    def build(self):
-        
-        youtube_url = 'http://youtube.com/watch?v=2lAe1cqCOXo'
-        
-        
-        layout = BoxLayout(orientation='vertical')
-        source = self.get_youtube_video_url(youtube_url)
-        video = VideoPlayer(source=source, state='play', options={'allow_stretch': True})
-        layout.add_widget(video)
-        return layout
+class MainScreen(Screen):
+    def __init__(self, **kwargs):
+        super(MainScreen, self).__init__(**kwargs)
+
+    def play_video(self):
+        sourse = self.ids.inpt.text
+        if sourse:
+            url, title = self.get_youtube_video_url(sourse)
+            self.ids.video_player.source = url
+            print(title)
+            self.ids.lbl.text = title
 
     def get_youtube_video_url(self, youtube_url):
         yt = YouTube(youtube_url)
+        title = yt.title
         video_stream = yt.streams.filter(res='720p').first()
-        return video_stream.url
+        return video_stream.url, title
+
+
+
+
+        #self.ids.video_player.source = url
+
+
+
+class MyApp(App):
+    def build(self):
+
+        return MainScreen()
+
+
+
+
 
 if __name__ == '__main__':
-    YouTubePlayerApp().run()
+    MyApp().run()
 
